314492b78ac305fc3dca13cf995b2dae96b873ee
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-12-13 22:35:26 -0500

    create rating form server action

--- src/app/ui/RatingModal.js
+++ src/app/ui/RatingModal.js
@@ -1,11 +1,12 @@
+'use client';
+import { useState } from "react";
 import StarRating from "./StarRating"
 
 export default function RatingModal(){
-
   return (
     <div className="RatingModal">
-      <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>RATE</button>
-      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+      <button className="btn" onClick={()=>document.getElementById('ratingModal').showModal()}>RATE</button>
+      <dialog id="ratingModal" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg"></h3>
           <StarRating />
--- src/app/ui/StarRating.js
+++ src/app/ui/StarRating.js
@@ -24,33 +24,31 @@ export default function StarRating() {
 
   for(let i = 0; i < 5; i++){
     stars.push(
-      <ul className="menu menu-horizontal bg-base-100 rounded-box">
         <li key={i} onClick={()=>clickHandler(i)}>
           <a>
             <Image src={i <= rating ? "/starGold.png" : "/starBlank.png"} height={30} width={30} alt={'star rating'}/>
           </a>
         </li>
-      </ul>
     )
   }
 
   return (
     <>
-    {stars}
+    <ul className="menu menu-horizontal bg-base-100 rounded-box">
+      {stars}
+    </ul>
     <button className="btn btn-square btn-ghost btn-xs" onClick={clearRating}>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
     </button>
     <form action={formAction}>
-      <input type="number" value={rating+1} hidden="true" name="rating"/>
+      <input type="number" value={rating+1} hidden={true} name="rating" readOnly={true}/>
       <h1>rate this</h1>
       <div className="flex flex-row">
         <textarea className="textarea textarea-secondary grow" placeholder="do it slap?" name="comments"></textarea>
       </div>
       <div className="modal-action">
         <button className="btn btn-success">Save</button>
-        <form method="dialog">
-          <button className="btn">Close</button>
-        </form>
+        <button className="btn" onClick={()=>document.getElementById('ratingModal').close()}>Close</button>
       </div>
     </form>
     </>
